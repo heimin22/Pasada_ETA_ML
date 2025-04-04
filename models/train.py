098bea8e721a3dffa6e90836109a3816a5dcc333
heimin22 <fyketonel@gmail.com>
2025-04-04 23:42:55 +0800

    Added and coded the evaluate.py for training evaluations

--- models/train.py
+++ models/train.py
@@ -10,3 +10,19 @@ def clean_data(df):
     df = df.dropna()
     return df
 
+def engineer_features(df):
+    # TODO: dito dapat yung feature engineering logic ko sa susunod
+    df['hour'] = pd.to_datetime(df['timestamp']).dt.hour
+    return df
+
+def save_processed_data(df):
+    output_path = Path(__file__).parent.parent / 'data' / 'processed' / 'processed_data.csv'
+    df.to_csv(output_path, index=False)
+
+if __name__ == '__main__':
+    print("Preprocessing data ")
+    raw_df = load_raw_data()
+    cleaned_df = clean_data(raw_df)
+    processed_df = engineer_features(cleaned_df)
+    save_processed_data(processed_df)
+    print("Data preprocessing completed!")
